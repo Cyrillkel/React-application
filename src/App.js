2d1a1637661638879bedb56079ca42fbef429d1d
Kirill Fedorov <cyrillkel@gmail.com>
2022-08-29 17:49:31 +0300

    created array and applied the map method

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "./components/elements/card.jsx";
+import { products } from "../src/menuList.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="main">
+      <div className="container">
+        <header className="header">
+          <h1>наша продукция</h1>
+          <div className="header__block">
+            <div className="header__busket">
+              <p className="header__title-busket">3 товара</p>
+              <p className="header__subtitle-busket">на сумму 3 500 ₽</p>
+            </div>
+            <img
+              className="header__icon-busket"
+              src="images/busket.svg"
+              alt="icon"
+            />
+          </div>
+        </header>
+
+        <div className="menu">
+          {products.map((key) => {
+            const { url, title, description, price, weight, icon } = key;
+            return (
+              <Card
+                url={url}
+                title={title}
+                description={description}
+                price={price}
+                weight={weight}
+                icon={icon}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </main>
   );
 }
 
