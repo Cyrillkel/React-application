6a80d6659b02af79c2d6a2ac3f3d8c7ed83e4b76
Kirill Fedorov <cyrillkel@gmail.com>
2022-09-04 03:29:03 +0300

    created second display

---
+++ src/orderList.js
@@ -0,0 +1,22 @@
+export const order = [
+  {
+    img: "images/Order_1.png",
+    title: "Устрицы по рокфеллеровски",
+    price: "2700 ₽",
+    icon: "images/order.svg",
+  },
+  {
+    img: "images/Order_2.png",
+    title: "Креветки по-королевски в лимонном соке",
+    price: "1800 ₽",
+    icon: "images/order.svg",
+  },
+  {
+    img: "images/Order_3.png",
+    title: "Свинные ребрышки на гриле с зеленью",
+    price: "1600 ₽",
+    icon: "images/order.svg",
+  },
+];
+
+export default order;
